940dc6b8ca8b12310acb726249959a309f555a05
ArjunPavanje <arjupavanje@gmail.com>
2025-05-07 15:55:48 +0530

    test case passed

--- FinalProject/digital_clock.v
+++ FinalProject/digital_clock.v
@@ -4,19 +4,22 @@
   * Mode 2: See time (24hr) and Date
   * Mode 3: Set Time, Date Manually
   * Mode 4: Set Alarm
-  * Mode 5: Set Timer
+  * Mode 5: Set Timer 
+  * Mode 6: Resetting Date and Time to 1 Jan 2024 12 AM
   * Input Formats:
     * Mode 3: 3 <HH> <MM> <SS> <DD>  <MM> <YY>
     * Mode 1: 1 
     * Mode 2: 2 
     * Mode 4: 4 <HH> <MM> <SS> (in 24 hr format)
     * Mode 5: 5 <MM> <SS> 
+    * Mode 6: 6
       */
 
      `timescale 1s/1ms
 
      module digital_clock(
        input clk,
+       // For Mode 3 (Setting Time, Date manually)
        input reset,
        input [4:0] set_hour,
        input [5:0] set_min,
@@ -25,16 +28,20 @@
        input [3:0] set_month,
        input [11:0] set_year,
 
+       // For Mode 4 (Alarm)
        input [4:0] alarm_hour,
        input [5:0] alarm_min,
        input [5:0] alarm_sec,
        input alarm_enable,
 
+       // For Mode 5 (Timer)
        input [5:0] timer_min,
        input [5:0] timer_sec,
-       input timer_start,
+       input timer_enable,
 
+       // Time Related
        output reg [4:0] hour,
+       output reg AM_enabled,
        output reg [5:0] min,
        output reg [5:0] sec,
        output reg [4:0] day,
@@ -56,10 +63,13 @@
          hour = 0; min = 0; sec = 0;
          day = 1; month = 1; year = 2020;
          alarm_buzzer = 0;
+         AM_enabled = 0;
          timer_buzzer = 0;
          timer_count_sec = 0;
          timer_count_min = 0;
        end
+
+       // To calculate day (given date, month, year)
        function [2:0] calc_day_of_week;
          input [4:0] d;
          input [3:0] m;
@@ -69,15 +79,15 @@
          begin
            yy = y;
            mm = m;
-           if (mm < 3) begin
+           if (mm < 3) begin 
              mm = mm + 12;
              yy = yy - 1;
-           end
+           end // Calculating using Zeller's Congruence  
            calc_day_of_week = ((d + (13*(mm+1))/5 + yy + (yy/4) - (yy/100) + (yy/400))-1) % 7;
          end
        endfunction
 
-
+       // Calculating number of days in a month
        function [5:0] days_in_month;
          input [3:0] m;
          input [11:0] y;
@@ -92,14 +102,7 @@
        endfunction
 
        always @(posedge clk or posedge reset) begin 
-         day_of_week <= calc_day_of_week(day, month, year);
-         // Ensuring that alarm and timer only ring ONCE
-         if (timer_buzzer) begin
-           timer_buzzer <= 0; // Turning the buzzer OFF
-         end
-         if (alarm_buzzer) begin 
-           alarm_buzzer <= 0; // Turning the buzzer OFF
-         end
+         
          if (reset) begin // Manual Reset (Mode 3)
            hour <= set_hour;
            min <= set_min;
@@ -118,7 +121,19 @@
            // due to non blocking statements used which are read at the next
            // positive edge of the clock
            sec <= sec + 1;
-           if (sec == 59) begin
+           // AM_enabled <= (hour < 12); 
+           if(hour == 23 && min == 59 && sec == 59) AM_enabled = 1; 
+           else AM_enabled = (hour < 12);
+
+           // Manual reset after april 2025 to january 2020
+           if (day == 30 && month == 4 && year == 2025 && hour == 23 && min == 59 && sec == 59) begin 
+             day <= 1;
+             month <= 1; 
+             year <= 2020;
+             hour <= 0; 
+             min <= 0;
+             sec <= 0;
+           end else if (sec == 59) begin
              sec <= 0;
              min <= min + 1;
              if (min == 59) begin
@@ -139,6 +154,27 @@
              end
            end
          end
+         // Updating Day
+         if (hour == 23 && min == 59 && sec == 59 && day != 30 && month != 4 && year != 2025) begin 
+           day_of_week <= calc_day_of_week(day+1, month, year);
+         end else if ( hour == 23 && min == 59 && sec == 59 && day == 30 && month == 4 && year == 2025) begin 
+           day_of_week <= calc_day_of_week(1, 1, 2020);
+           end else if ( hour == 23 && min == 59 && sec == 59 && day == 28 && month == 2 && days_in_month(month, year) == 29) begin 
+           day_of_week <= calc_day_of_week(29, 2, year);
+           end else if ( hour == 23 && min == 59 && sec == 59 && day == 28 && month == 2 && days_in_month(month, year) == 28) begin 
+           day_of_week <= calc_day_of_week(1, 3, year);
+           end
+           else begin  
+           day_of_week <= calc_day_of_week(day, month, year);
+         end
+         // Ensuring that alarm and timer only ring ONCE
+         if (timer_buzzer) begin
+           timer_buzzer <= 0; // Turning the buzzer OFF
+         end
+         if (alarm_buzzer) begin 
+           alarm_buzzer <= 0; // Turning the buzzer OFF
+         end
+
          //$display("Sanity Check: Time: %02d %02d %02d, Alarm TIme: %02d %02d %02d", hour, min, sec, alarm_hour, alarm_min, alarm_sec);
          // Triggering Alarm
          if (alarm_enable && !alarm_triggered && alarm_hour == 0 && alarm_min == 0 && alarm_sec == 0 && 
@@ -152,7 +188,7 @@
          end
 
          // Timer Check
-         if (timer_start && !timer_running) begin
+         if (timer_enable && !timer_running) begin
            timer_count_min <= timer_min;
            timer_count_sec <= timer_sec;
            timer_running <= 1;
@@ -163,7 +199,7 @@
            if (timer_count_min == 0 && timer_count_sec == 2) begin
              timer_buzzer <= 1;
              timer_running <= 0; // Turning it OFF 
-             timer_count_sec = 0; // Manually resetting timer to 00 00 again
+             timer_count_sec <= 0; // Manually resetting timer to 00 00 again
            end else begin // Decrementing (minutes/seconds) otherwise
              if (timer_count_sec == 0) begin
                if (timer_count_min > 0) begin
--- FinalProject/testbench.v
+++ FinalProject/testbench.v
@@ -19,8 +19,9 @@ reg [5:0] alarm_min = 0, alarm_sec = 0;
 reg alarm_enable = 0;
 
 reg [5:0] timer_min = 0, timer_sec = 0;
-reg timer_start = 0;
+reg timer_enable = 0;
 
+wire AM_enabled;
 wire [4:0] hour;
 wire [5:0] min, sec;
 wire [4:0] day;
@@ -34,18 +35,18 @@ wire alarm_buzzer, timer_buzzer;
 integer file, mode, res;
 integer display_mode = 2;
 integer ticks_after_event = 0;
-assign timer_count_min = 6'd0;
-assign timer_count_sec = 6'd0;
+//assign timer_count_min = 6'd0;
+//assign timer_count_sec = 6'd0;
 
 // Instantiating Module of Clock
 digital_clock uut (
-  .clk(clk), .reset(reset),
+  .clk(clk), .reset(reset), .AM_enabled(AM_enabled),
   .set_hour(set_hour), .set_min(set_min), .set_sec(set_sec),
   .set_day(set_day), .set_month(set_month), .set_year(set_year),
   .alarm_hour(alarm_hour), .alarm_min(alarm_min), .alarm_sec(alarm_sec),
   .alarm_enable(alarm_enable),
   .timer_min(timer_min), .timer_sec(timer_sec),
-  .timer_start(timer_start),
+  .timer_enable(timer_enable),
   .hour(hour), .min(min), .sec(sec),
   .day(day), .month(month), .year(year), .day_of_week(day_of_week),
   .timer_count_min(timer_count_min), .timer_count_sec(timer_count_sec),
@@ -58,17 +59,9 @@ always #0.5 clk = ~clk;
 // Setting Clock cycle as 0.5 seconds as changes are only read at positive edge of clock 
 // (if clock period was kept as 1 second, changes would be read every two seconds instead of 1)
 
-//integer timer_done = 0; 
 // Reading data from text file for which Mode to follow
 initial begin 
-  day_names[0] = "Sunday";
-  day_names[1] = "Monday";
-  day_names[2] = "Tuesday";
-  day_names[3] = "Wednesday";
-  day_names[4] = "Thursday";
-  day_names[5] = "Friday";
-  day_names[6] = "Saturday";
-  file = $fopen("input.txt", "r");
+    file = $fopen("input.txt", "r");
   if (!file) begin
     $display("Cannot open input.txt");
     $finish;
@@ -76,12 +69,17 @@ initial begin
 
   while (!$feof(file)) begin
     res = $fscanf(file, "%d", mode); 
+    day_names[0] = "Sunday";
+    day_names[1] = "Monday";
+    day_names[2] = "Tuesday";
+    day_names[3] = "Wednesday";
+    day_names[4] = "Thursday";
+    day_names[5] = "Friday";
+    day_names[6] = "Saturday";
 
     // If Mode is either 1 or 2, displaying time (5 times) in 12 hour or
     // 24 hour format respectively
     if (mode == 1 || mode == 2) begin
-      //$display("---- Mode %0d ----", mode);
-      //display_mode = mode; 
       repeat (5) begin
         #1;
         // Checking if timer or alarm rings while the repeat block is
@@ -93,14 +91,13 @@ initial begin
         end 
         if (timer_buzzer) begin // It is an 'if' statement, not an 'else if' as both a timer and an alarm may be active
           $display("TIMER DONE!");
-          timer_start = 0; // Turning the timer OFF
-          //timer_done = 1;
+          timer_enable = 0; // Turning the timer OFF
           ticks_after_event = 0;
         end if (mode == 1) begin // 12 hour time format
           $display("Time: %02d:%02d:%02d %s | Date: %02d-%02d-%04d (%s)",
             (hour == 0) ? 12 : (hour > 12 ? hour - 12 : hour),
             min, sec,
-            (hour >= 12) ? "PM" : "AM", // AM or PM
+            (AM_enabled) ? "AM" : "PM", // AM or PM
             day, month, year, day_names[day_of_week]);
         end else begin // 24 hour time format
           $display("Time: %02d:%02d:%02d | Date: %02d-%02d-%04d (%s)",
@@ -118,10 +115,22 @@ initial begin
       res = $fscanf(file, "%d %d %d", alarm_hour, alarm_min, alarm_sec);
       alarm_enable = 1; // Turning the alarm ON
       $display("Alarm set for %02d:%02d:%02d", alarm_hour, alarm_min, alarm_sec);
-    end else if (mode == 5) begin // Setting timer
+    end else if (mode == 5 && !timer_enable) begin // Setting timer
       res = $fscanf(file, "%d %d", timer_min, timer_sec);
+      timer_enable = 1; // Turning the timer ON
       $display("Current time: %02d : %02d : %02d, Timer set for %02d minutes %02d seconds", hour, min, sec, timer_min, timer_sec);
-      timer_start = 1; // Turning the timer ON
+    end else if(mode == 5 && timer_enable) begin
+    end else if (mode == 6) begin 
+        set_hour = 0;
+    set_min = 0;
+    set_sec = 0;
+    set_day = 1;
+    set_month = 1;
+    set_year = 2020;
+    reset = 1; #0.5; reset = 0;
+    $display("RESET! Time: %02d:%02d:%02d | Date: %02d-%02d-%04d (%s)",
+            hour, min, sec, day, month, year, day_names[day_of_week]);
+
   end else begin
     $display("Unknown mode: %d", mode);
   end
@@ -129,7 +138,6 @@ initial begin
 end
 $fclose(file);
 
-// Step 2: Monitor and display time
 repeat (300) begin
   #1; 
   if (alarm_buzzer) begin // Ringing alarm (when it occurs outside Mode 1 or 2)
@@ -138,32 +146,29 @@ repeat (300) begin
     ticks_after_event = 0;
   end if (timer_buzzer) begin  // Ringing timer (when it occurs outside Mode 1 or 2)
     $display("TIMER DONE! Time: %02d:%02d:%02d | Date: %02d-%02d-%04d", hour, min, sec, day, month, year);
-    timer_start = 0; // Turning the timer OFF
-    //timer_done = 1;
+    timer_enable = 0; // Turning the timer OFF
     ticks_after_event = 0;
-  end else if (timer_count_min || timer_count_sec) begin
+  end /*else if (timer_count_min || timer_count_sec) begin
     $display("TIMER: %02d:%02d", timer_count_min, timer_count_sec);
     ticks_after_event = 0;
-  end else begin // Displaying time in either 24 hour or 12 hour format
-    if (!alarm_enable) begin
-
-    end
-    else if (display_mode == 1)
+  end*/ 
+  if(alarm_buzzer == 0 && timer_buzzer == 0) begin // Displaying time in either 24 hour or 12 hour format
+    if (display_mode == 1)
       $display("Time: %02d:%02d:%02d %s | Date: %02d-%02d-%04d",
     (hour == 0) ? 12 : (hour > 12 ? hour - 12 : hour),
     min, sec,
-    (hour >= 12) ? "PM" : "AM",
+    (AM_enabled) ? "AM" : "PM",
     day, month, year);
-  else
-    $display("Time: %02d:%02d:%02d | Date: %02d-%02d-%04d (%s)",
-  hour, min, sec, day, month, year, day_names[day_of_week]);
-ticks_after_event = ticks_after_event + 1;
-end
+    else
+      $display("Time: %02d:%02d:%02d | Date: %02d-%02d-%04d (%s)",
+    hour, min, sec, day, month, year, day_names[day_of_week]);
+    ticks_after_event = ticks_after_event + 1;
+  end
 //$display("Sanity Check: alarm buzzer: %d, timer buzzer: %d, timer: %d: %d, alarm enabled? %d, ticks after event: %d", alarm_buzzer, timer_buzzer, timer_count_min, timer_count_sec, alarm_enable, ticks_after_event);
 // The below line is to ensure that as long as timer/alarm is active program
 // continues to show time even though Mode 1 or Mode 2 is not specified
 // suffeciently for timer/alarm to run down and ring.
-if (!alarm_buzzer && !timer_buzzer && timer_count_min == 0 && timer_count_sec == 0 &&
+if (!alarm_buzzer && !timer_buzzer && !timer_enable &&
   ticks_after_event > 5 && !alarm_enable) begin
   $display("Simulation complete.");
   $finish;
